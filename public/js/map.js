77d70f3420d4463d6acd42b3f6c864a4e2e75b9a
Mushirul Hassan <mushirulhassan2@gmail.com>
2025-06-09 23:05:22 +0530

    Updated project

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,16 @@
 
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
-    console.log(coordinates);
+    // console.log(coordinates);
 
      const marker = new mapboxgl.Marker()
-        .setLngLat(coordinates) //Listing.geometry.coordinates
+        .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
+        .setPopup( new mapboxgl.Popup({offset: 25})
+    
+    .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
         .addTo(map);
